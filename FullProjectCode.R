bee7465c24b635faca046f88278881b4afe6094b
Adam Hinthorne <ajh12014@mymail.pomona.edu>
2018-05-15 12:29:26 -0700

    Data scrape and hit scatters

--- FullProjectCode.R
+++ FullProjectCode.R
@@ -1,3 +1,239 @@
+###FULL PROJECT CODE###
+
+##DATA COLLECTION##
+
+##D-III data scraping from Gabe Chandler, updated for new website configuration##
+#background picture for visualizations
+field <- readPNG("baseballfieldbnw.PNG")
+
+#innings of a baseball game
+innings<-c("1","2","3","4","5","6","7","8","9")
+
+#pick website to scrape from 
+website<-c("http://www.sagehens.com/sports/bsb/2016-17/schedule")
+
+require(XML)
+require(RCurl)
+require(stringr)
+require(png)
+
+#Build functions, find.box.scores pulls information from URL
+find.box.scores <- function(website) {
+  url <- getURL(website)
+  boxscore <- gregexpr("boxscores", url)[[1]]
+  href <- gregexpr("href=", url)[[1]]
+  good.href <- c()
+  for (i in 1:length(boxscore)){
+    good.href[i] <- max((href<boxscore[i])*href)}
+  quotes <- gregexpr('"', url)[[1]]
+  boxes <- c()
+  for (i in 1:length(good.href))
+    boxes <- rbind(boxes, quotes[min(which(quotes>good.href[i])):(min(which(quotes>good.href[i]))+1)])  
+  site <- str_sub(website, 1, gregexpr("edu", website)[[1]][1]+2)
+  site2 <- str_sub(website, 1, gregexpr("com", website)[[1]][1]+2)
+  if (nchar(site2) < nchar(site) | nchar(site2) > 13)
+    site <- site2
+  urls.temp <- c()
+  for (i in 1:nrow(boxes)) {
+    urls.temp [i] <- str_c(str_c(site, str_sub(url, boxes[i,1]+1, boxes[i,2]-1), sep=""), "", sep="")
+  }
+  
+  urls<-c()
+  for (i in 1:nrow(boxes)){
+    urls[i] <- str_c(urls.temp[i], "?view=plays&inning=1","",sep="")
+  }
+  
+  pbp <- list()
+  for (i in 1:length(urls)) {
+    pbp[[i]] <- getURL(urls[i])
+    trunc <- gregexpr("Top of         1st",pbp[[i]])
+    pbp[[i]] <- str_sub(pbp[[i]],trunc[[1]][1],nchar(pbp[[i]]))
+  }
+  return(pbp)
+}
+
+
+#pulls table of all hits from find.box.scores output
+get.table <- function(name, playbyplays) {
+  actions <- c("grounded out to", "popped up to", "flied out to", "lined out to", "reached first on an error by", "lined into double play", "grounded into double play")
+  positions <- c('p', 'c ', '1b', '2b', '3b', 'ss', 'lf', 'cf', 'rf')
+  m <- 1
+  outcomes <- c()
+  locations <- c()
+  occurences <- c()
+  for (i in 1:length(actions)) {
+    for (j in 1:length(positions)) {
+      count <- 0
+      for (k in 1:length(playbyplays)) {
+        count <- count + length(gregexpr(str_c(name, str_c(actions[i], positions[j], sep=" "), sep=" "), playbyplays[[k]])[[1]])*(gregexpr(str_c(name, str_c(actions[i], positions[j], sep=" "), sep=" "), playbyplays[[k]])[[1]][1]!=-1)
+      }
+      outcomes[m] <- actions[i]
+      locations[m] <- positions[j]
+      occurences[m] <- count
+      m <- m + 1
+    }
+  }
+  actions <- c("singled", "doubled", "tripled", "homered")
+  positions <- c("to left field", "up the middle", "through the left side", "to right field", "through the right side", "to left center", "to right center", "down the left field line", "down the right field line", "to third base", "to short stop", "to second base", "to first base", "to center field", "down the lf line", "down the rf line")
+  m <- 1
+  outcomes1 <- c()
+  locations1 <- c()
+  occurences1 <- c()
+  for (i in 1:length(actions)) {
+    for (j in 1:length(positions)) {
+      count <- 0
+      for (k in 1:length(playbyplays)) {
+        count <- count + length(gregexpr(str_c(name, str_c(actions[i], positions[j], sep=" "), sep=" "), playbyplays[[k]])[[1]])*(gregexpr(str_c(name, str_c(actions[i], positions[j], sep=" "), sep=" "), playbyplays[[k]])[[1]][1]!=-1)
+      }
+      outcomes1[m] <- actions[i]#str_c(actions[i], positions[j], sep=" ")
+      locations1[m] <- positions[j]
+      occurences1[m] <- count
+      m <- m + 1
+    }
+  }
+  return(data.frame(outcomes = c(outcomes, outcomes1), locations=c(locations, locations1), occurences=c(occurences, occurences1)))
+}
+
+table.output <- function(dataframe) {
+  n <- nrow(dataframe)
+  m <- sum(dataframe[,3])
+  stuff <- matrix(0,nrow=m, ncol=2)
+  stuff <- as.data.frame(stuff)
+  names(stuff) <- c('out', 'pos')
+  increments <- c(0, cumsum(dataframe[which(dataframe[,3]>0),3]))
+  j <- 1
+  for (i in which(dataframe[,3]>0)) {
+    stuff$out[(increments[j]+1):m] <- as.character(dataframe[i,1])
+    stuff$pos[c(increments[j]+1):m] <- as.character(dataframe[i,2])
+    j <- j + 1
+  }
+  return(table(stuff))
+}
+
+####################
+### Sample Usage to get table for player
+####################
+
+website <-"http://www.sagehens.com/sports/bsb/2016-17/schedule"
+names <- c("T. Nishioka")
+playbyplays <- find.box.scores(website)
+
+
+
+#if playbyplays is not playbyplay information, alter findboxscores (lines 21-23)
+#look through playbyplays to find all inconsistencies in how names are written and fix them
+for (i in 1:length(playbyplays)) {
+  playbyplays[[i]] <- gsub("Dunn,Connor", "Dunn", playbyplays[[i]])
+}
+
+###Data collection from Dan Malter Github page
+
+library(readr)
+spraycharts <- read_csv("https://raw.githubusercontent.com/danmalter/Spray-Chart/master/spraycharts.csv")
+View(spraycharts)
+
+#The spraychart is built on a 250x250 grid. y values are flipped, with home plate at the bottom. Flip this value (-y+250) for better visualization
+library(dplyr)
+spraycharts<-spraycharts%>%mutate(z=-y+250)
+#Cut down data to what we need, save spraycharts for reference
+spraycharts2<-spraycharts%>%dplyr::select("Description","x","z","type","batter.name")
+
+##Defining each hit type
+#Densities for Each Hit Type
+library(dplyr)
+library(ggplot2)
+#Types of Hits/Outs
+positions <- c("to left field", "up the middle", "through the left side", "to right field", "through the right side", "to left center", "to right center", "down the left field line", "down the right field line", "to third base", "to short stop", "to second base", "to first base", "to center field", "down the lf line", "down the rf line")
+hitscatter<-spraycharts%>%filter(type=="H")%>%filter(Description!="Home Run")%>%filter(y>=25)%>%ggplot(aes(x=x,y=-y+250,color=Description,ymin=25,ymax=245))+geom_jitter()
+hitscatter
+
+##hit scatter five breaks up each section of the field through the play by plays
+hitscatter2<-hitscatter+geom_point(aes(x=125,y=35),color="black")+geom_abline(slope=-1,intercept=160)+geom_abline(slope=1,intercept=-90)
+hitscatter3<-hitscatter2+geom_abline(slope=-1.25,intercept=191.25)+geom_abline(slope=1.25,intercept=-121.25)
+hitscatter4<-hitscatter3+geom_abline(slope=-2.25,intercept=316.25)+geom_abline(slope=2.25,intercept=-246.25)+geom_abline(slope=-5.5,intercept=722.5)+geom_abline(slope=5.5,intercept=-652.5)
+hitscatter4+geom_point(aes(x=125,y=200),color="black")+geom_point(aes(x=125,y=135),color="black")
+hitscatter5<-hitscatter4+geom_abline(slope=-.5,intercept=207.5)+geom_abline(slope=.5,intercept=82.5)
+hitscatter5
+
+##hit charts fro all play by plays
+single.lf<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+single.rf<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)
+single.cf<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+single.triangle1<-spraycharts2%>%filter(Description=="Single")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+single.triangle2<-spraycharts2%>%filter(Description=="Single")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+single.cf2<-bind_rows(single.cf,single.triangle1,single.triangle2)
+single.utm<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+single.lcf<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+single.rcf<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+single.lfl<-spraycharts2%>%filter(Description=="Single")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(-1.25*x)+191.25)
+single.rfl<-spraycharts2%>%filter(Description=="Single")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(1.25*x)-121.25)
+single.ss<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+single.2b<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+single.3b<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+single.1b<-spraycharts2%>%filter(Description=="Single")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)
+single.tls<-bind_rows(single.3b,single.ss)
+single.trs<-bind_rows(single.2b,single.1b)
+singles.of<-bind_rows(single.lfl,single.lf,single.lcf,single.cf2,single.rcf,single.rf,single.rfl)
+
+double.lf<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+double.rf<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)
+double.cf<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+double.triangle1<-spraycharts2%>%filter(Description=="Double")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+double.triangle2<-spraycharts2%>%filter(Description=="Double")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+double.cf2<-bind_rows(double.cf,double.triangle1,double.triangle2)
+double.utm<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+double.lcf<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+double.rcf<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+double.lfl<-spraycharts2%>%filter(Description=="Double")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(-1.25*x)+191.25)
+double.rfl<-spraycharts2%>%filter(Description=="Double")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(1.25*x)-121.25)
+double.ss<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+double.2b<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+double.3b<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+double.1b<-spraycharts2%>%filter(Description=="Double")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)
+double.tls<-bind_rows(double.3b,double.ss)
+double.trs<-bind_rows(double.2b,double.1b)
+doubles.of<-bind_rows(double.lf,double.rf,double.cf2,double.lcf,double.rcf,double.lfl,double.rfl)
+
+triple.lf<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+triple.rf<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)
+triple.cf<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+triple.triangle1<-spraycharts2%>%filter(Description=="Triple")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+triple.triangle2<-spraycharts2%>%filter(Description=="Triple")%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z>=(.5*x)+82.5)
+triple.cf2<-bind_rows(triple.cf,triple.triangle1,triple.triangle2)
+triple.utm<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(5.5*x)-652.5)%>%filter(z>=(-5.5*x)+722.5)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(.5*x)+82.5)
+triple.lcf<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z>=(.5*x)+82.5)
+triple.rcf<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z>=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+triple.lfl<-spraycharts2%>%filter(Description=="Triple")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(-1.25*x)+191.25)
+triple.rfl<-spraycharts2%>%filter(Description=="Triple")%>%filter(z>=25)%>%filter(x>=5)%>%filter(z<=(1.25*x)-121.25)
+triple.ss<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z<=(-5.5*x)+722.5)%>%filter(z>=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+triple.2b<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)%>%filter(z<=(5.5*x)-652.5)
+triple.3b<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(x>=5)%>%filter(z>=(-1.25*x)+191.25)%>%filter(z<=(-2.25*x)+316.25)%>%filter(z<=(.5*x)+82.5)
+triple.1b<-spraycharts2%>%filter(Description=="Triple")%>%filter(z<=235)%>%filter(z>=(1.25*x)-121.25)%>%filter(z<=(2.25*x)-246.25)%>%filter(z<=(-.5*x)+207.5)
+triple.tls<-bind_rows(triple.3b,triple.ss)
+triple.trs<-bind_rows(triple.2b,triple.1b)
+triples.of<-bind_rows(triple.lf,triple.rf,triple.cf2,triple.lcf,triple.rcf,triple.lfl,triple.rfl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###Practice Matrix
 x<-c(runif(10,0,10))
 y<-c(runif(10,0,10))
